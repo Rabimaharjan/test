243b35ec8a01daa2409b58e37cd5fdf1334f82be
Rabi Maharjan <rabi.maharjan@gmail.com>
2017-07-15 12:59:09 +0100

    Inital commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+require 'users/user.php';
+require 'users/developer.php';
+
+
+
+$user = new User();
+$user->name = 'Rabi Maharjan';
+//$user->title = 'Mr';
+$user->setTitle('mr');
+$user->setDob('January 2 2018');
+
+echo $user->getSalutation();
+echo "<br />\n";
+echo $user->getDob();
+echo "<br />\n";
+
+$james = new Developer();
+$james->name = 'James Smith';
+echo $james->getSalutation();
+echo "<br />\n";
+
+$james->setSkills(['Beard','Zend Framework','PHP']);
+echo $james->getSkillsString();
+
+$james->setSkills(['Java','Cpp']);
+echo $james->getSkillsString();
+
+$james->setTools('eat');
+echo $james->showTools();
+
+echo "git test";
+?>
---
+++ users/toolkit.php
@@ -0,0 +1,31 @@
+<?php
+
+	trait ToolKit
+	{
+		private $tools = array();
+		
+		public function setTools($task)
+		{
+			switch($task){
+				case "eat":
+					$this->tools[] = "Spoon";
+					$this->tools[] = "Fork";
+					$this->tools[] = "Knife";
+					
+					break;
+					
+			}
+			return $this;
+		}
+		
+		public function getTools(){
+			return $this->tools;
+		}
+		
+		public function showTools(){
+			return implode(", ",$this->tools);
+		}
+	
+	}
+
+?>
